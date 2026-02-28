5a4a32aa7ddb2ee579b77bae952bcada925a8605
me15degrees <me15degrees@gmail.com>
2026-02-28 08:20:18 -0300

    feat: adjust frames for a lower resolution

--- detectar_libras_celular.py
+++ detectar_libras_celular.py
@@ -8,7 +8,7 @@ import sys
 USE_WEBCAM = False 
 
 # Se USE_WEBCAM for False, mude o IP abaixo
-URL_CELULAR = "http://192.168.100.99:8080/video" 
+URL_CELULAR = "http://192.168.100.155:8080/video" 
 
 try:
     model = joblib.load('libras_rf_model.pkl')
@@ -43,55 +43,41 @@ mp_drawing = mp.solutions.drawing_utils
 
 print("Câmera iniciada. Pressione 'q' para sair.")
 
+LARGURA_PROC = 480  
+ALTURA_PROC = 360
+
 while True:
     ret, frame = cap.read()
     if not ret:
-        print("Erro ao capturar frame. Verifique a conexão da câmera.")
         break
 
-    # --- ROTACIONAR 90 GRAUS PARA DIREITA ---
-    frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
-    # ----------------------------------------
+    frame = cv2.resize(frame, (LARGURA_PROC, ALTURA_PROC))
+
+    frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE) #rotação
 
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
     results = hands.process(frame_rgb)
 
     letra_predita = "" 
 
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
-            
-            mp_drawing.draw_landmarks(
-                frame, 
-                hand_landmarks, 
-                mp_hands.HAND_CONNECTIONS
-            )
+            # Desenhar apenas se necessário (consome CPU)
+            mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
             data_row = []
             for landmark in hand_landmarks.landmark:
                 data_row.extend([landmark.x, landmark.y, landmark.z])
 
             landmarks_array = np.array([data_row]) 
-
             prediction_numeric = model.predict(landmarks_array) 
-            
             letra_predita = le.inverse_transform(prediction_numeric)[0] 
     
-    cv2.rectangle(frame, (10, 10), (160, 110), (0, 0, 0), -1)
-    
-    cv2.putText(
-        frame, 
-        letra_predita, 
-        (30, 90), 
-        cv2.FONT_HERSHEY_SIMPLEX, 
-        3, 
-        (255, 255, 255), 
-        4, 
-        cv2.LINE_AA
-    )
+    # Interface gráfica simplificada
+    cv2.putText(frame, f"Letra: {letra_predita}", (10, 50), 
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-    cv2.imshow('Detector de LIBRAS (Fase 3)', frame)
+    cv2.imshow('Orange Pi LIBRAS', frame)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
